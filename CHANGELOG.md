6d2578112adcdb4c9dfc6a12b37e42eb5f7d17b8
nlf <quitlahok@gmail.com>
2021-09-09 12:41:41 -0700

    feat: refactor to do far less work
    BREAKING CHANGE: no longer copies files or runs commands, only sets up
    the package.json and informs the user to install the template package

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
 # Changelog
 
+2.0.0 - more minimal, now only provides initial package.json prompts and logs manual steps to run.
 1.0.0 - first implementation. creates a package.json, copies files, and runs commands.
---
+++ test/prompt.js
@@ -0,0 +1,58 @@
+const sinon = require('sinon')
+const t = require('tap')
+
+const read = sinon.stub()
+const prompt = t.mock('../lib/prompt.js', { read })
+
+t.afterEach(() => {
+  read.reset()
+})
+
+t.test('can provide custom prompt', async (t) => {
+  const response = 'yes'
+  const mock = read.withArgs(sinon.match({
+    prompt: 'supports custom prompts:',
+  })).yields(null, response)
+
+  const promptFn = prompt({
+    prompt: 'supports custom prompts:',
+  })
+
+  const result = await promptFn('IGNORED', '/')
+  t.equal(result, response)
+  t.ok(mock.called)
+})
+
+t.test('can provide default as a value', async (t) => {
+  const response = 'default'
+  const mock = read.withArgs(sinon.match({
+    prompt: 'value:',
+    default: response,
+  })).yields(null, response)
+
+  const promptFn = prompt({
+    default: response,
+  })
+
+  const result = await promptFn('value', '/')
+  t.equal(result, response)
+  t.ok(mock.called)
+})
+
+t.test('can provide default as a function', async (t) => {
+  const response = 'default'
+  const defaultFn = sinon.stub().returns(response)
+  const mock = read.withArgs(sinon.match({
+    prompt: 'value:',
+    default: response,
+  })).yields(null, response)
+
+  const promptFn = prompt({
+    default: defaultFn,
+  })
+
+  const result = await promptFn('value', '/')
+  t.equal(result, response)
+  t.ok(mock.called)
+  t.ok(defaultFn.calledWith('value', '/'), 'passed parameters to default fn')
+})
